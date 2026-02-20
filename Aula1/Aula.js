c42391c0d0b56e011703e8f6ae1b77f44a5c4c84
VNcin94 <games.xdvini@gmail.com>
2026-02-20 16:58:59 -0300

    feat:Aula 1 Adicionada

---
+++ Aula1/Aula.js
@@ -0,0 +1,107 @@
+1.//let nome = "Vinicin"
+//let curso = "JS"
+//console.log("Olá", nome + "bem-vindo ao curso de", curso + "!");
+
+2.//let valor = 9;
+//valor = valor * 7;
+
+//console.log(valor);
+
+3.//let pontos = 100;
+//let bonus = pontos * 0.2;
+//let total = pontos + bonus;
+
+//console.log("Pontuação final: " + total);
+
+4.//let idade = 15;
+
+//if (idade >= 18) {
+// console.log("Acesso liberado");
+//} else {
+// console.log("Acesso bloqueado: menor de idade");
+//}
+
+
+5.//let temperatura = 30;
+
+//if (temperatura > 30) {
+ // console.log("Está calor!");
+//} else if (temperatura >= 15 && temperatura <= 30) {
+//  console.log("Clima agradável!");
+//} else {
+//  console.log("Está frio!");
+//}
+
+6. //let senhaCorreta = "jra";
+//let tentativa = "jra"; // muda aqui pra testar
+
+//if (tentativa === senhaCorreta) {
+//  console.log("Login realizado!");
+//} else {
+ // console.log("Senha incorreta");
+//}
+
+//for(let i = 10; i>= 0; i--){
+
+//console.log(i)
+
+//}
+
+
+
+//const m = (a, b) => {return a * b};
+
+//let n = n(5, 6)
+
+//console.log(n)
+
+//function saudacao(nome, curso) {
+ // console.log("Olá " + nome + ", bem-vindo ao curso de " + curso + "!");
+//}
+
+//saudacao("Vinicin", "JS");
+
+//function multiplicar(valor) {
+ // return valor * 7;
+//
+
+//let resultado = multiplicar(50000000000);
+//console.log(resultado);
+
+//function calcularPontuacao(pontos) {
+ // let bonus = pontos * 0.2;
+//  let total = pontos + bonus;
+//  return total;
+//}
+//let resultado = calcularPontuacao(100);
+//console.log("Pontuação final: " + resultado);
+
+
+//function verificarAcesso(idade) {
+ // if (idade >= 18) {
+  //  console.log("Acesso liberado");
+ // } else {
+   // console.log("Acesso bloqueado: menor de idade");
+  //}
+//}
+
+//verificarAcesso(80000000);
+
+//function olharClima(temperatura) {
+//  if (temperatura > 30) {
+//    console.log("Está calor!");
+//  } else if (temperatura >= 15 && temperatura <= 30) {
+//    console.log("Clima agradável!");
+ // } else {
+//    console.log("Está frio!");
+//  }
+//}
+//olharClima(555555555555555550);
+
+function contagemRegressiva() {
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+contagemRegressiva();
